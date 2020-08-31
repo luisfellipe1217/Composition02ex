8de3633e6b7dd3feba6601e1d048c0722c2dde0c
luisfellipe1217 <luis.fellipe1217@gmail.com>
2020-08-31 10:18:26 -0300

    Criado Classe Post

--- src/Entities/Comment.java
+++ src/Entities/Comment.java
@@ -17,6 +17,6 @@ public class Comment {
 
     public void setText(String text) {
         this.text = text;
-    }
+    }  
 
 }
---
+++ src/Entities/Post.java
@@ -0,0 +1,70 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class Post {
+    
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer likes;
+
+    private List<Comment> comments = new ArrayList<>();
+    
+    public Post () {    
+    }
+
+    public Post(Date moment, String title, String content, Integer likes) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+    
+    public void addComment(Comment comment){
+       comments.add(comment);
+    }
+    
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+    }
+}
